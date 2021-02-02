3476ad6d359a305f9efdd44dac700fab68d0994f
lnealer <lrosenealer@gmail.com>
2021-02-02 13:17:48 -0600

    script complete

--- scripts/recon.py
+++ scripts/recon.py
@@ -8,26 +8,69 @@ class Recon:
         i = 1
         d0_pos = Recon.input
         while not d0_pos[i].startswith("D1"):
-            if d0_pos[i].startswith("D0") or not recon_in[i].split():
-                i = i + 1
-                continue
             cur = d0_pos[i].split()
+            if not cur or d0_pos[i].startswith("D0"):
+                i += 1
+                continue
             stock = cur[0]
-            shares = cur[1]
+            shares = float(cur[1])
             Recon.out[stock] = shares
-            i = i+1
+            i += 1
         Recon.index = i+1
 
-    def update(self, recon_in):
+
+    def update(self):
         i = Recon.index
-        while (d1_trn[i] != "D1_POS"):
+        d1_trn = Recon.input
+        while not d1_trn[i].endswith("POS"):
             cur = d1_trn[i].split()
+            if not cur :
+                i += 1
+                break
             stock = cur[0]
             trn = cur[1]
-            shares = cur[2]
-            val = cur[3]
+            shares = float(cur[2])
+            val = float(cur[3])
+            if stock not in Recon.out:
+                Recon.out[stock] = 0
+            if (trn == "SELL"):
+                Recon.out["Cash"] += val
+                Recon.out[stock] -= shares
+            elif (trn == "BUY"):
+                Recon.out["Cash"] -= val
+                Recon.out[stock] += shares
+            elif (trn == "DEPOSIT"):
+                Recon.out["Cash"] += val
+            elif (trn == "FEE"):
+                Recon.out["Cash"] -= val
+            elif (trn == "DIVIDEND"):
+                Recon.out["Cash"] += val
+            i += 1
+        Recon.index = i+1
     
-    #def compare(self, recon_in) 
+    def compare(self):
+        out = open("recon.out", "w+")
+        d1_trn = Recon.input
+        i = Recon.index
+        length = len(Recon.input)
+        while i < length:
+            cur = d1_trn[i].split()
+            stock = cur[0]
+            shares = float(cur[1])
+            if stock not in Recon.out:
+                diff = shares
+            else: 
+                diff = shares - Recon.out[stock]
+                del Recon.out[stock]
+            if diff:
+                out.write(stock + " " + str(diff) + "\n")
+            i += 1
+        for stock in Recon.out:  
+            diff = -1*Recon.out[stock]
+            if diff:
+                out.write(stock + " " + str(diff) + "\n")
+        out.close() 
+         
 
 
 
@@ -36,4 +79,6 @@ if __name__ == '__main__':
     f = open(recon_in, 'r')
     lines = f.readlines()
     recon_out = Recon(lines)
+    recon_out.update()
+    recon_out.compare()
     
